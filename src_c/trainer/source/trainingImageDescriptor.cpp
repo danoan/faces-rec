b280ebb5a7cef4376a2fa7185300a3e24ef891d6
daniel <danoan2008@gmail.com>
2014-08-07 00:30:54 -0300

    Conserto de bugs em trainingImageDescriptor

--- src_c/trainer/source/trainingImageDescriptor.cpp
+++ src_c/trainer/source/trainingImageDescriptor.cpp
@@ -1,5 +1,7 @@
 #include "../headers/trainingImageDescriptor.h"
 
+int TID::prime_numbers[146] = {83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541,547,557,563,569,571,577,587,593,599,601,607,613,617,619,631,641,643,647,653,659,661,673,677,683,691,701,709,719,727,733,739,743,751,757,761,769,773,787,797,809,811,821,823,827,829,839,853,857,859,863,877,881,883,887,907,911,919,929,937,941,947,953,967,971,977,983,991,997};
+
 void TID::init(){	
 	if(_crop_selector){
         getSize(_img_path.c_str(), &_size);
@@ -19,7 +21,7 @@ void TID::init(){
     }	
 }
 
-void TID::choose_random_hop(int total_crops){
+int TID::choose_random_hop(int total_crops){
     for(int i=0;i<145;i++){
         if(total_crops%TID::prime_numbers[i]!=0){
             return TID::prime_numbers[i];
@@ -60,9 +62,9 @@ int TID::loadNextCrops(int n, int& totalRead, int& theEnd, int(* checkData)(ulon
     int total_crops;
 
     if(_img_data==NULL){
-        total_crops = getImageCrops(&data,&_img_data,&_img_size,_img_path.c_str(),&_last_crop,n,_max_crops,_crop_size.x,_crop_size.y,_shift_step,random_hop,checkData,vp);
+        total_crops = getImageCrops(&data,&_img_data,&_img_size,_img_path.c_str(),&_last_crop,n,_max_crops,_crop_size.x,_crop_size.y,_shift_step,_random_hop,checkData,vp);
     }else{
-        total_crops = getImageCrops(&data,&_img_data,&_img_size,&_last_crop,n,_max_crops,_crop_size.x,_crop_size.y,_shift_step,random_hop,checkData,vp,0);
+        total_crops = getImageCrops(&data,&_img_data,&_img_size,&_last_crop,n,_max_crops,_crop_size.x,_crop_size.y,_shift_step,_random_hop,checkData,vp,0);
     }
     for(int i=0;i<total_crops;i++){
         _crops.push_back(data[i]);;
